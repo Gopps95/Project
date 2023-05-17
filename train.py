a67dd16d01ec7e2a4231dfdb140dc801987e9b4a
gopps95 <shankargopi7@gmail.com>
2023-05-17 21:40:24 +0530

    chaange intents file

--- train.py
+++ train.py
@@ -11,16 +11,19 @@ from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.models import load_model
 
+nltk.download('wordnet')
+
 lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('C:/Users/Asus/OneDrive/Desktop/CollegeProject/Project/dev_light.json').read())
+intents = json.loads(open(r'C:\Users\Asus\OneDrive\Desktop\CollegeProject\Project\intents.json').read())
+
 
 words = []
-classes = []
+classes = []  
 documents = []
 ignore_letters = ['?', '!', '.', ',', '\n', '\t']
 
-for intent in intents:
+for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
@@ -28,4 +31,34 @@ for intent in intents:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+classes = sorted(set(classes))
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(words,open('classes.pkl','wb'))
+
+training = []
+output_empty = [0]* len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+
+
+output_row = list(output_empty)
+output_row[classes.index(document[1])] = 1
+training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:,1])
+
+model = Sequential()
+model.add(Dense(128,input_shape=(len(train_x[0]),),activation='relu'))
+model.add(Dropout(0.5))
